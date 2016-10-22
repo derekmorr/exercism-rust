84545bdeb520760d940f5a6b44941d128e23c52c
Derek Morr <derek.morr.test@gmail.com>
2016-10-21 20:45:36 -0400

    inital acronym; not fully working

---
+++ acronym/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn abbreviate(s: &str) -> String {
+    fn process_word(w: &str) -> char {
+        w.chars().nth(0).unwrap().to_uppercase().nth(0).unwrap()
+    }
+
+    let words = s.split_whitespace();
+    words.fold("".to_string(), |mut acc, e| { acc.push(process_word(e)); acc } )
+}
